98c885e68ab46020de47c0e1c429f20b1bee7fed
Mehul Patel <mehul8@gmail.com>
2013-10-26 12:54:47 -0400

    finished indexOf

--- src/underbar.js
+++ src/underbar.js
@@ -53,6 +53,13 @@ var _ = { };
     // TIP: Here's an example of a function that needs to iterate, which we've
     // implemented for you. Instead of using a standard `for` loop, though,
     // it uses the iteration helper `each`, which you will need to write.
+    var output;
+    _.each(array, function(value, index, array){
+      if (target === value){
+        output = output || +index;
+      }
+    });
+    return output || -1;
   };
 
   // Return all elements of an array that pass a truth test.
