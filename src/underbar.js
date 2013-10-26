be731b433dc6a05e429777f3cbefcd751061603e
Mehul Patel <mehul8@gmail.com>
2013-10-26 17:26:43 -0400

    worked on uniq, can only get it to work with sorted arrays for some reason.

--- src/underbar.js
+++ src/underbar.js
@@ -88,6 +88,13 @@ var _ = { };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var output = [];
+    _.each(array, function(value, index, array){
+      if(_.indexOf(output, value) == -1){
+        output.push(value);
+      }
+    });
+    return output;
   };
 
 
