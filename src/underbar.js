b8d60054b4ef5fd6808a77abd2d45d2cd93ca5e9
Mehul Patel <mehul8@gmail.com>
2013-10-27 17:49:54 -0400

    _.extend passes tests

--- src/underbar.js
+++ src/underbar.js
@@ -226,6 +226,12 @@ var _ = { };
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(arguments, function(thingsToAdd){
+      for(var key in thingsToAdd){
+        obj[key] = thingsToAdd[key];
+      }
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
